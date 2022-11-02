75c0144a94b65de46f24eb85c42d676e641ee48c
btcslg <76098091+btcslg@users.noreply.github.com>
2022-11-02 10:13:34 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -96,9 +96,8 @@ cat >"$CLEAR_PATH" <<-EOF
 packages
 config.buildinfo
 feeds.buildinfo
-openwrt-x86-64-generic-kernel.bin
-openwrt-x86-64-generic.manifest
-openwrt-x86-64-generic-squashfs-rootfs.img.gz
 sha256sums
 version.buildinfo
+ipk.tar.gz
+manifest
 EOF
